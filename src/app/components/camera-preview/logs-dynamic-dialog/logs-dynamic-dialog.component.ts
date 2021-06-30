65588d3934acd0df5f90bc814ab77ef2ed897c97
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-06-30 12:47:00 +0200

    dialog update

--- src/app/components/camera-preview/logs-dynamic-dialog/logs-dynamic-dialog.component.ts
+++ src/app/components/camera-preview/logs-dynamic-dialog/logs-dynamic-dialog.component.ts
@@ -1,5 +1,6 @@
 import { LogsModel } from './../../interfaces/logsModel';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-logs-dynamic-dialog',
@@ -7,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs-dynamic-dialog.component.scss'],
 })
 export class LogsDynamicDialogComponent implements OnInit {
-  logs!: LogsModel[];
+  logs: LogsModel[] = [
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '12_12_12' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+    { filename: 'elo.mp4', time: '11_13_14' },
+  ];
   loading: boolean = true;
 
   constructor() {}
 
   ngOnInit() {}
+
+  @ViewChild('t') t: Table | undefined;
+
+  public applyFilterForTable($event: any, val: string): void {
+    this.t!.filterGlobal(($event.target as HTMLInputElement).value, val);
+  }
 }
