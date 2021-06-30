d9b8b3248be4b6f0878b5edcf3c296d415440742
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2021-06-30 16:32:32 +0200

    update

--- src/app/components/camera-preview/areas-dynamic-dialog/areas-dynamic-dialog.component.ts
+++ src/app/components/camera-preview/areas-dynamic-dialog/areas-dynamic-dialog.component.ts
@@ -1,7 +1,6 @@
 import { AreaModel } from './../../interfaces/areaModel';
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
-import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-areas-dynamic-dialog',
@@ -9,16 +8,11 @@ import { Table } from 'primeng/table';
   styleUrls: ['./areas-dynamic-dialog.component.scss'],
 })
 export class AreasDynamicDialogComponent {
-  @ViewChild('t') t: Table | undefined;
   logs!: AreaModel[];
   loading: boolean = true;
 
   constructor(private config: DynamicDialogConfig) {
     this.logs = config.data;
     this.loading = false;
-  }
-
-  public applyFilterForTable($event: any, val: string): void {
-    this.t!.filterGlobal(($event.target as HTMLInputElement).value, val);
   }
 }
